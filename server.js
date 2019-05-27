4b9533c1b6afb1bf927d3dd0915e2d730621aec3
Sohieeb <ibrahim.abdelrahman95@gmail.com>
2019-05-27 09:51:28 +0300

    fixing deploy error

--- server.js
+++ server.js
@@ -28,14 +28,15 @@ var sharejs = require('share');
 
 // set up redis server
 var redisClient;
-console.log(process.env.REDISTIGI_URL);
-if (process.env.REDISTIGI_URL) {
-    var rtg = require("url").parse(process.env.REDISTIGI_URL);
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
     redisClient = require("redis").createClient(rtg.port, rtg.hostname);
     redisClient.auth(rtg.auth.split(":")[1]);
 } else {
     redisClient = require("redis").createClient();
 }
+
 
 
 // options for sharejs
